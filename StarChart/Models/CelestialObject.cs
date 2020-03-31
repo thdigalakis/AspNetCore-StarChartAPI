fd4ab1c5717d19ddc6629142ece1fcff93b472b1
Thodoris Digalakis <th.digalakis@gmail.com>
2020-03-31 22:02:53 +0300

    Fix property in CelestialObject

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -15,5 +15,6 @@ namespace StarChart.Models
         public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
     }
 }
